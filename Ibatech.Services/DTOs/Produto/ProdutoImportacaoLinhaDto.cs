7f1a2b5281ed352de12e6179f6e63c347c4e2739
Jonatas Bento <pr.jmbento@gmail.com>
2026-07-10 15:57:14 -0300

    feat: adiciona MiniExcel e modelo bruto de importacao

---
+++ Ibatech.Services/DTOs/Produto/ProdutoImportacaoLinhaDto.cs
@@ -0,0 +1,15 @@
+namespace Ibatech.Services.DTOs.Produto;
+
+internal sealed class ProdutoImportacaoLinhaDto
+{
+    public object? Nome { get; init; }
+    public object? Tipo { get; init; }
+    public object? PrecoCompra { get; init; }
+    public object? PrecoVenda { get; init; }
+    public object? QuantidadeInicial { get; init; }
+    public object? QuantidadeMinima { get; init; }
+    public object? CodigoSku { get; init; }
+    public object? Descricao { get; init; }
+    public object? Marca { get; init; }
+    public object? Modelo { get; init; }
+}
